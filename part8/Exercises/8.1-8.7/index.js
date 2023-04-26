0a597386cc0e7f7cc28a0851f47617f9b94ab994
ZoranUTF8 <janjiczoran23@gmail.com>
2023-04-26 12:16:00 +0900

    finix graphql first exercise

--- part8/Exercises/8.1-8.7/index.js
+++ part8/Exercises/8.1-8.7/index.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
+const { GraphQLError } = require("graphql");
 
 let authors = [
   {
@@ -91,20 +93,30 @@ const typeDefs = `
   type Author {
     name:String!,
     born:Int,
-    id:ID!
+    id:ID!,
+    bookCount: Int!
   }
 
-  type Book{
+  type Book {
     title:String!,
     published:Int!,
-    author:String!,
+    author: String!,
     genres: [String!]!
     }
-
-  
-  type Query{
+  type Query {
     authorCount:Int!
     bookCount:Int!
+    allBooks(author: String,genre: String):[Book]!
+    allAuthors: [Author!]!
+  }
+  type Mutation {
+    addBook(
+      title:String!,
+      published:Int!,
+      author: String!,
+      genres: [String!]!
+    ):Book
+    editAuthor(name:String!,birthYear:Int!):Author
   }
 `;
 
@@ -112,6 +124,76 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
+    allBooks: (root, args) => {
+      switch (true) {
+        case Boolean(args.author):
+          const allAuthorBooks = books.filter(
+            (book) => book.author === args.author
+          );
+          return allAuthorBooks;
+        case Boolean(args.genre):
+          const allGenreBooks = books.filter((book) =>
+            book.genres.includes(args.genre)
+          );
+          return allGenreBooks;
+        default:
+          return books;
+      }
+    },
+    allAuthors: () => {
+      return authors.map((author) => {
+        const bookCount = books.filter(
+          (book) => book.author === author.name
+        ).length;
+        return { ...author, bookCount };
+      });
+    },
+  },
+
+  Mutation: {
+    addBook: (root, args) => {
+      const { title, published, author, genres } = args;
+      // check if author already exists in authors array
+      let existingAuthor = authors.find((a) => a.name === author);
+
+      // if author doesn't exist, add the author to the authors array
+      if (!existingAuthor) {
+        const newAuthor = { name: author, id: uuid() };
+        authors = authors.concat(newAuthor);
+        existingAuthor = newAuthor;
+      }
+
+      //  than add the book as well
+      const book = {
+        title,
+        published,
+        author: existingAuthor.name,
+        genres,
+        id: uuid(),
+      };
+      books = books.concat(book);
+      return book;
+    },
+    editAuthor: (root, args) => {
+      // check if author already exists in authors array
+      let existingAuthor = authors.find((a) => a.name === args.name);
+      console.log(authors);
+      if (!existingAuthor) {
+        throw new GraphQLError("No such author found.", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.name,
+          },
+        });
+      }
+
+      const updatedAuthor = { ...existingAuthor, born: args.birthYear };
+
+      authors = authors.map((author) =>
+        author.name === args.name ? updatedAuthor : author
+      );
+      return updatedAuthor;
+    },
   },
 };
 
@@ -131,6 +213,60 @@ get author count
 query {
   authorCount
 }
+
+get all books
+query {
+  allBooks { 
+    title 
+    author
+    published 
+    genres
+  }
+}
+
+get all authors
+query {
+  allAuthors {
+    name
+    bookCount
+  }
+}
+
+get all authors with books count
+query {
+  allAuthors {
+    name
+    bookCount
+  }
+}
+
+get books from the author
+query {
+  allBooks(author: "Robert Martin") {
+    title
+  }
+}
+
+get books by the genre
+query {
+  allBooks(genre: "refactoring") {
+    title
+    author
+  }
+}
+
+ add new book mutation {
+  addBook(
+    title: "NoSQL Distilled",
+    author: "Martin Fowler",
+    published: 2012,
+    genres: ["database", "nosql"]
+  ) {
+    title,
+    author
+  }
+}
+
 
 
 */
