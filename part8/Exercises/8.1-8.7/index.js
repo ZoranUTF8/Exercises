0b223e47266ac7407bbca35be530bdc06935d4f0
ZoranUTF8 <janjiczoran23@gmail.com>
2023-05-02 20:19:01 +0900

    Add mongodb server to backend

--- part8/Exercises/8.1-8.7/index.js
+++ part8/Exercises/8.1-8.7/index.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
+
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 const { v1: uuid } = require("uuid");
 const { GraphQLError } = require("graphql");
+
 
 let authors = [
   {
--- part8/Practice/backend/index.js
+++ part8/Practice/backend/index.js
@@ -1,30 +1,24 @@
+require("dotenv").config();
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
-const { v1: uuid } = require("uuid");
 const { GraphQLError } = require("graphql");
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    phone: "040-123543",
-    street: "Tapiolankatu 5 A",
-    city: "Espoo",
-    id: "3d594650-3436-11e9-bc57-8b80ba54c431",
-  },
-  {
-    name: "Matti Luukkainen",
-    phone: "040-432342",
-    street: "Malminkaari 10 A",
-    city: "Helsinki",
-    id: "3d599470-3436-11e9-bc57-8b80ba54c431",
-  },
-  {
-    name: "Venla Ruuska",
-    street: "Nallemäentie 22 C",
-    city: "Helsinki",
-    id: "3d599471-3436-11e9-bc57-8b80ba54c431",
-  },
-];
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const Person = require("./models/person");
+
+const MONGODB_URI = process.env.TEST_MONGODB_URI_GRAPHQL;
+
+console.log("connecting to", MONGODB_URI);
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connection to MongoDB:", error.message);
+  });
 
 const typeDefs = `
 
@@ -39,7 +33,7 @@ enum YesNo {
   }
   type Person {
     name: String!
-    phone: String
+    phone: String!
     address: Address!
     id: ID!
   }
@@ -65,49 +59,63 @@ enum YesNo {
 
 const resolvers = {
   Query: {
-    personCount: () => persons.length,
-    allPersons: () => persons,
-    findPerson: (root, args) => persons.find((p) => p.name === args.name),
-    allPersonsWithPhone: (root, args) => {
-      const byPhone = (person) =>
-        args.phone === "YES" ? person.phone : !person.phone;
-      console.log(`By phone is ${byPhone}`);
-      return persons.filter(byPhone);
+    personCount: async () => Person.collection.countDocuments(),
+    allPersons: async (root, args) => {
+      if (!args.phone) {
+        return Person.find({});
+      }
+      return Person.find({ phone: { $exists: args.phone === "YES" } });
     },
+    findPerson: async (root, args) => Person.findOne({ name: args.name }),
   },
   Person: {
-    address: ({ street, city }) => {
+    address: (root) => {
       return {
-        street,
-        city,
+        street: root.street,
+        city: root.city,
       };
     },
   },
 
   Mutation: {
-    addPerson: (root, args) => {
-      if (persons.find((p) => p.name === args.name)) {
-        throw new GraphQLError("Name must be unique", {
+    addPerson: async (root, args) => {
+      const person = new Person({ ...args });
+
+      try {
+        await person.save();
+      } catch (error) {
+        throw new GraphQLError("Saving person failed", {
           extensions: {
             code: "BAD_USER_INPUT",
             invalidArgs: args.name,
+            error,
           },
         });
       }
-
-      const person = { ...args, id: uuid() };
-      persons = persons.concat(person);
       return person;
     },
-    editNumber: (root, args) => {
-      const person = persons.find((p) => p.name === args.name);
-      if (!person) {
-        return null;
-      }
+    editNumber: async (root, args) => {
+      const person = await Person.findOne({ name: args.name });
 
-      const updatedPerson = { ...person, phone: args.phone };
-      persons = persons.map((p) => (p.name === args.name ? updatedPerson : p));
-      return updatedPerson;
+      if (!person) {
+        throw new GraphQLError("No such person found.", {
+          extensions: { code: "BAD_USER_INPUT", invalidArgs: args.name },
+        });
+      } else {
+        try {
+          person.phone = args.phone;
+          await person.save();
+        } catch (error) {
+          throw new GraphQLError("Saving number failed", {
+            extensions: {
+              code: "BAD_USER_INPUT",
+              invalidArgs: args.name,
+              error,
+            },
+          });
+        }
+      }
+      return person;
     },
   },
 };
